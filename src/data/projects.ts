261118175a11b2370765ebbca84afd09112aedf8
Leo Silva <leandromarcelotorressilva@gmail.com>
2025-05-29 14:02:11 +0200

    project updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,7 +6,7 @@ export const projects: Project[] = [
     title: "QR Code Generator",
     description: "A versatile QR code generator that allows users to create custom QR codes with various data types and styles.",
     image: "/images/screencapture-localhost-5173-2025-05-29-13_53_08.png",
-    tags: ["React", "Node.js", "Vite", "Tailwind CSS", "TypeScript"],
+    tags: ["React", "Node.js", "Tailwind CSS", "TypeScript"],
     demoUrl: "https://qr-code-generator-sooty-two.vercel.app/",
     githubUrl: "https://github.com/NAUTBOL/qr-code-generator"
   }
